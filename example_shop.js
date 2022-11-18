22f5cbf9cd38316824cabf0fe370a951638e5a5d
mborland <mkborland@aol.com>
2022-11-18 14:43:21 -0600

    added example shop for Ross

---
+++ example_shop.js
@@ -0,0 +1,30 @@
+({
+  name: 'Conjure',
+  desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  projects: [
+    {
+      name: 'Tether',
+      tags: [
+        'Javascript',
+        'Web-app',
+        'UDL (unified data library)',
+        'GPS (global positioning system)',
+        'Flight-data',
+        'Data-collection',
+        'Mission planning',
+        'Information display',
+        'Planes',
+        'AMC (Air Mobility Command)',
+      ],
+      lang: ['Javascript'],
+      proj_link: 'tbd',
+      short_desc:
+        'Tether is a microservice that linked the (MEIS) databases with the Unified Data Library (UDL) for the purposes of transporting AMC aircraft positional data. The MEIS was only visible to AMC mission planners, so Tether provided that data to nine additional COCOMS. Tether allowed mission planners to coordinate sorties and refueling operations with other aircraft platforms aligned to MAJCOMs outside of AMC.',
+      img: 'tether',
+    },
+  ],
+  __v: 0,
+  location: 'Scott AFB, Illinois',
+  img: 'Conjure',
+  contact: 'aflcmc.hbmc.conjure@us.af.mil',
+});
